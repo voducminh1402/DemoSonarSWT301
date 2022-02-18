b24dc03f6f311c72568d3fa80c5d4805c471c409
voducminh1402 <voducminh140201@gmail.com>
2022-02-18 09:58:24 +0700

    commit base

---
+++ src/demo/gui/DemoSWT.java
@@ -0,0 +1,25 @@
+package demo.gui;
+
+public class DemoSWT {
+	private String name;
+	private int age;
+	
+	public DemoSWT(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
